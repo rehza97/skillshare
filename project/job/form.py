abb1ca5d06464f0eaa807804c36302b58d691264
fethallah <fethotebib3@gmail.com>
2024-04-16 01:10:12 +0100

    quick fix

--- project/job/form.py
+++ project/job/form.py
@@ -3,12 +3,12 @@ from .models import Job , CateFeed , ReviewRating
 class JobFormCreation(forms.ModelForm):
     class Meta:
         model = Job
-        fields=['title','Wilaya','banner','image1','image2','image3','image4', 'description','price','is_available','category']
+        fields=['title','Wilaya','banner','image1','image2','image3','image4', 'description','price','is_available','category',]
         
 class JobFormUpdate(forms.ModelForm):
     class Meta:
         model = Job
-    fields=['title','Wilaya','banner','image1','image2','image3','image4', 'description','price','is_available','category']        
+        fields=['title','Wilaya','banner','image1','image2','image3','image4', 'description','price','is_available','category',]        
 class categoryFeedBackForm(forms.ModelForm):
     class Meta:
         model = CateFeed
